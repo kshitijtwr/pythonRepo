75a764d4cb10aff39d78d4391497414866814dd1
kshitijtwr <tiwarikshitij@gmail.com>
2025-06-01 13:36:07 +0530

    code changes

---
+++ app.py
@@ -0,0 +1,10 @@
+print(1+1)
+#Python testing the code
+
+age = 30
+if age > 44:
+    print(age)
+elif age == 30:
+    print("Age is equal")    
+else:
+    print("you are not 44") #this will print "you are not 44"
